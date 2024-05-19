3085d5495b67a4ffe09cde5c6edbf29e0772857d
nacho <nachoagusss1@gmail.com>
2024-05-19 20:42:53 -0300

    detalle en url de fligthHistory

--- django-project/apps/fligthHistory/urls.py
+++ django-project/apps/fligthHistory/urls.py
@@ -4,5 +4,5 @@ from .views import SearchFligth, FligthQuery
 
 urlpatterns = [
     path('searchFligth/', SearchFligth.template, name='search_fligth'),
-    path('queryFligth/', FligthQuery.template , name='search_fligth'),
+    path('queryFligth/', FligthQuery.template , name='query_fligth'),
 ]
