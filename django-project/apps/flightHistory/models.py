611a67664dba6b288cd4d69e00fbaf3fbbb59d31
CardenasTiago <cardenas.tiago.r@gmail.com>
2024-06-04 20:47:39 -0300

    Se modifico la tabla flightHistory

--- django-project/apps/flightHistory/models.py
+++ django-project/apps/flightHistory/models.py
@@ -5,6 +5,7 @@ from .manager import FlightHistoryManager
 class FlightHistory(models.Model):
     date = models.DateField()
     fk_plane = models.ForeignKey(Plane, on_delete=models.CASCADE)
+    sold_ticket = models.IntegerField(null=False)
 
     objects = FlightHistoryManager()
 
