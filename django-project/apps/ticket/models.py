dbb116fcdb6119c41e0580f1aa21646166f96ac4
Nacho Sanchez <nachoagusss1@gmail.com>
2024-06-05 18:02:59 -0300

    se actualizo el __str__ de ticket

--- django-project/apps/ticket/models.py
+++ django-project/apps/ticket/models.py
@@ -15,4 +15,4 @@ class Ticket(models.Model):
     objects = TicketManager()
 
     def __str__(self) -> str:
-        return f"{self.flight_date} - {self.ticket_class} - {self.purchase_date} - {self.seat_location} - {self.fk_user.name} - {self.fk_user.last_name} - {self.fk_user.dni} - {self.fk_flight.code} - {self.fk_flight.departure_time} - {self.fk_flight.fk_route.fk_airport_departure.airport_code} - {self.fk_flight.fk_route.fk_airport_arrival.airport_code}"
+        return f" {self.ticket_class} - {self.purchase_date} - {self.seat_location} - {self.fk_user.first_name} - {self.fk_user.last_name} - {self.fk_user.dni} - {self.fk_flight.code} - {self.fk_flight.departure_time} - {self.fk_flight.fk_route.fk_airport_departure.airport_code} - {self.fk_flight.fk_route.fk_airport_arrival.airport_code}"  
