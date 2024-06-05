97ec349f134a1859e294ebc04b58ad7861b11e11
CardenasTiago <cardenas.tiago.r@gmail.com>
2024-06-05 16:57:15 -0300

    Se modifico la tabla Route

--- django-project/apps/route/models.py
+++ django-project/apps/route/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from .manager import RouteManager
+from apps.airport.models import Airport
 
 class Route(models.Model):
-    fk_airport_departure = models.ForeignKey('self', on_delete=models.CASCADE, null= False,related_name='departure_routes')
-    fk_airport_arrival = models.ForeignKey('self', on_delete=models.CASCADE, null= False,related_name='arrival_routes')
+    fk_airport_departure = models.ForeignKey(Airport, on_delete=models.CASCADE, null= False,related_name='departure_routes')
+    fk_airport_arrival = models.ForeignKey(Airport, on_delete=models.CASCADE, null= False,related_name='arrival_routes')
 
     objects = RouteManager()
 
