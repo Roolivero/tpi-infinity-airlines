80caba28c96d9782c68f5ef432090e45827046f7
nacho <nachoagusss1@gmail.com>
2024-06-08 16:05:37 -0300

    se implemento la verificacion de que el usuario este autenticado antes de comprar un ticket

--- django-project/apps/ticket/views.py
+++ django-project/apps/ticket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.flightHistory.models import FlightHistory
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Ticket
 from .forms import PurchaseTicketForm
 from datetime import timezone
@@ -11,6 +12,12 @@ from datetime import timezone
 class PurchaseTicket():
     def template(request, flight_id):
         flight = get_object_or_404(FlightHistory, id=flight_id)
+        
+        user = request.user
+        if not user.is_authenticated:
+            messages.info(request, "Debe iniciar sesión para comprar un ticket.")
+            request.session['flight_id'] = flight_id
+            return redirect("login")
         
         flight.available_tickets = FlightHistory.objects.calculate_available_tickets(flight)
 
