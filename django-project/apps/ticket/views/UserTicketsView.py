2929d9713b9c6bae37bfbbd79e3382b4f467ed82
CardenasTiago <cardenas.tiago.r@gmail.com>
2024-06-12 17:20:06 -0300

    Se arreglao my_tickets

--- django-project/apps/ticket/views/UserTicketsView.py
+++ django-project/apps/ticket/views/UserTicketsView.py
@@ -1,6 +1,7 @@
 from ..models import Ticket
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from datetime import date
 # Create your views here.
 
 
@@ -10,4 +11,9 @@ class UserTicketsView(LoginRequiredMixin, ListView):
     context_object_name = 'tickets'
 
     def get_queryset(self):
-        return Ticket.objects.for_user(self.request.user)
+        return Ticket.objects.for_user(self.request.user).select_related('fk_flight_history', 'fk_flight__fk_route__fk_airport_departure', 'fk_flight__fk_route__fk_airport_arrival')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['today'] = date.today()
+        return context
